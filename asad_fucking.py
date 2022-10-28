60a048970761485736ce7f28a89c7fc786e5a5cb
Soikot1m <112489925+Soikot1m@users.noreply.github.com>
2022-10-28 17:37:41 +0600

    Update asad_fucking.py

--- asad_fucking.py
+++ asad_fucking.py
@@ -1329,7 +1329,7 @@ def Subscraption():
 		print("")
 		print(" Copy And Send Key To Admin")
 		print ("")
-		print (" Your Key : "+fuck+asad+key1 )
+		print (" Your Key : "+ak+ah+key1 )
 		print ("")
 		name = input(" Your Name : ")
 		print ("")
